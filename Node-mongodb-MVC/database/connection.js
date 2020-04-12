2f03951b17400639685d7769ca666e3ec2288443
Shravan Zolgiker <M1047488@mindtree.com>
2020-04-12 20:55:14 +0530

    Working on Project 1

---
+++ Node-mongodb-MVC/database/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`DataBase Connected`);
+  } catch (error) {
+    console.log("Database Error", error);
+    throw new Error(error);
+  }
+};
---
+++ Node-mongodb-MVC/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotEnv = require("dotenv");
+const cors = require("cors");
+const dbConnection = require("./database/connection");
+dotEnv.config();
+
+const app = express();
+
+// DB Connectivity
+dbConnection();
+
+// CORS middleware
+app.use(cors());
+
+// Request Payload Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+  res.send("Hello from Express");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on PORT ${PORT}`);
+});
+
+//Error Handling Middleware Global
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send({
+    status: 500,
+    message: err.message,
+    body: "Something Broke!",
+  });
+});
